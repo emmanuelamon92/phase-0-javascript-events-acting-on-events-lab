a45f6ad4223db7be8d6529a16436c4e59c63f0a3
Emmanuel <emmanuelamon92@gmail.com>
2021-04-18 17:57:07 -0400

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+const dodger = document.getElementById("dodger"); 
+dodger.style.backgroundColor = "#FF69B4"; 
+dodger.style.bottom = "100px"; 
+dodger.style.bottom = "0px";
+// dodger.style.left = "0px"; 
+
+let moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+    
+let moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 440){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+
+// document.addEventListener("keydown", function(event) {
+//     console.log(event)
+//     if (event.key === "ArrowLeft") {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// });
